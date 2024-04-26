82cf830558cfcdc58f844efcc5603b542796a1f6
Vahigoku <106706476+Vahigoku@users.noreply.github.com>
2024-04-26 10:08:28 +0200

    uu

---
+++ ddueksamen/Assets/Scripts/Gun.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage;
+    public float range;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(fpsCam.)
+    }
+}
