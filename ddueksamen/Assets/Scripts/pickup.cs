6ef9d2bc9b44913bd85e2e2ff847cef497753c27
sebEFN <94054113+sebEFN@users.noreply.github.com>
2024-05-07 14:18:17 +0200

    pickup

---
+++ ddueksamen/Assets/Scripts/pickup.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    public GameObject player;
+    inventory myinven;
+
+    private void Start()
+    {
+        myinven = FindObjectOfType<inventory>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ("Player"))
+        {
+            Destroy(gameObject);
+            myinven.Keep();
+        }
+
+    }
+    void Update()
+    {
+       
+    }
+
+
+
+}
