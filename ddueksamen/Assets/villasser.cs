cc924311b0c3973f4563eb3aaf5d16d60b4da247
Vahigoku <vahigoku@yahoo.com>
2024-05-15 11:25:34 +0200

    derfd4f
    df

---
+++ ddueksamen/Assets/villasser.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class villasser : MonoBehaviour
+{
+    public bool maybe;
+    public GameObject dia;
+    public GameObject dia1;
+    teleport teleport;
+    inventory inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        teleport = FindObjectOfType<teleport>();
+        inventory = FindObjectOfType<inventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void OnTriggerStay(Collider other)
+    {
+       /* if (other.tag == ("Player") && Input.GetKey(KeyCode.E) && teleport.explored)
+        {
+            dia.SetActive(true);
+        }*/
+        if (other.tag == ("Player") && Input.GetKey(KeyCode.E) && teleport.explored && inventory.items.Contains("Chest Parent 001"))
+        {
+            dia1.SetActive(true);
+        }
+
+    }
+}
